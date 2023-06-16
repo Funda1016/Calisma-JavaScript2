f2efe80a057b39cd52bcc4214491c4f752b553bf
Funda1016 <110299963+Funda1016@users.noreply.github.com>
2023-06-15 22:29:13 -0700

    WorkingWithTeam

---
+++ javascript/calisma.js
@@ -0,0 +1,18 @@
+function YaziyiDegistir(){
+   document.getElementById('h3tag').innerHTML ="Merhaba";
+
+}
+function RenkDegis(){
+    document.getElementById('h3tag').style.color="red";
+
+}
+function Bir(){
+    document.getElementById('resim').src="image/Cicek1.jpeg";
+
+}
+function Iki(){
+    document.getElementById('resim').src="image/Cicek2.jpeg";
+}
+function Three(){
+    document.getElementById('resim').src="image/Cicek3.jpeg";
+}
